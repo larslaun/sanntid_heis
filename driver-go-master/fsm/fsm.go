cce9a8d78a1c0a415b058632742f34dd5e7a53f0
EvenFosby <even.fosby@gmail.com>
2024-01-30 21:18:00 +0100

    added fsm_onRequestButtonPress

--- driver-go-master/fsm/fsm.go
+++ driver-go-master/fsm/fsm.go
@@ -1,12 +1,17 @@
 package fsm
 
-import(
-
+import (
+	"Driver-go/elevio"
+	"driver-go-master/elevator"
+	"fmt"
 )
 
-elevatorOutputDevice outputDevice
-
-
 func fsm_onInitBetweenFloors(){
+	elevio.SetMotorDirection(elevio.MD_Down)
+	elevator.dirn = elevio.MD_Down
+	elevator.behaviour = elevator.EB_Moving
+}
 
+func fsm_onRequestButtonPress(btn_floor int, btn_type elevio.ButtonType){
+	fmt.Printf("\n\n%T(%T, %T)", "__FUNCTION__", btn_floor, )
 }
