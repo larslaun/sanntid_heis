1363595448e6cbd503f00f85bdffae3803bfacc3
EvenFosby <even.fosby@gmail.com>
2024-01-30 21:38:10 +0100

    changes to fsm

--- driver-go-master/fsm/fsm.go
+++ driver-go-master/fsm/fsm.go
@@ -1,12 +1,13 @@
 package fsm
 
-import(
-
+import (
+	"Driver-go/elevio"
+	"driver-go_master/elevator"
+	"driver-go_master/elevio"
 )
 
-elevatorOutputDevice outputDevice
-
-
 func fsm_onInitBetweenFloors(){
-
+	elevio.SetMotorDirection(elevio.MD_Down)
+	elevator.dirn = elevio.MD_Down
+	elevator.behaviour = elevator.EB_Moving
 }
