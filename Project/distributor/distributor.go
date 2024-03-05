920d2fa5ef7f234135953313f28a34d4e7c45c96
larslaun <125959619+larslaun@users.noreply.github.com>
2024-03-05 16:30:08 +0100

    trying to merge

--- Project/distributor/distributor.go
+++ Project/distributor/distributor.go
@@ -8,16 +8,12 @@ import (
 	"time"
 )
 
-
-func DistributeState(elevStateTx chan elevator.Elevator , localElev *elevator.Elevator){
+func DistributeState(elevStateTx chan elevator.Elevator, localElev *elevator.Elevator) {
 	for {
 		elevStateTx <- *localElev
 		time.Sleep(1000 * time.Millisecond)
 	}
 }
-
-
-
 
 //psuedo distributor
 //Recieves buttonpress, then calculates optimal elevator wiht cost func,then sends elevOrder which includes order and ID of elev.
@@ -29,8 +25,8 @@ func DistributeOrder(buttonPress chan elevio.ButtonEvent,  elevOrderTx chan coll
 		case a:=<-buttonPress:
 			elevOrder := chooseOptimalElev(buttonPress, elevators) //choose optimalelev must calculat cost func for all elevs and create order to optimal elevator
 			elevOrderTx<-elevOrder
+
 		}
 	}
 }
 */
-
