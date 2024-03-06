e461cfbebd91a8b3e5a4c8aacbbda63f70e00f2a
EvenFosby <evenfosby@gmail.com>
2024-03-06 11:18:53 +0100

    merge

--- Project/watchdog/watchdog.go
+++ Project/watchdog/watchdog.go
@@ -3,24 +3,34 @@ package watchdog
 
 import (
 	"Elev-project/driver-go-master/elevator"
-	"Elev-project/driver-go-master/elevio"
+	"Elev-project/driver-go-master/requests"
+	"Elev-project/settings"
 	"fmt"
+	"time"
 
 )
 
-const Time int = 3
 
-WatchdogTimer := time.NewTimer(Time * time.Second)
 
-/*
-Loop:
-	for {
-		select {
-		case a <- 
-			
-		case 
-			
+
+func LocalWatchdog(floors chan int, elev *elevator.Elevator, redistributeSignal chan bool){
+	watchdogTimer := time.NewTimer(settings.WatchdogTimeoutDuration * time.Second)
+	for{
+		select{
+		case <-watchdogTimer.C:
+			if requests.HasRequests(*elev){
+				redistributeSignal <- true
+				elev.Available = false
+			} else{
+				watchdogTimer.Reset(settings.WatchdogTimeoutDuration * time.Second)
+			}
+		case <-floors:
+			watchdogTimer.Reset(settings.WatchdogTimeoutDuration * time.Second)
+			fmt.Print("\nWatchdog reset\n")
+			elev.Available = true
 		}
 	}
+}
 
-*/  
+
+
